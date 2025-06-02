2878234d0ccd169eae6ed9cdcb845743076cd804
itmak-ru <am@itmak.ru>
2025-06-02 20:29:05 +0300

    Добавлена реализация на PowerShell

---
+++ fix_spooler_ps.ps1
@@ -0,0 +1,15 @@
+﻿# Принудительное завершение процессов
+Stop-Process -Name "spoolsv" -Force -ErrorAction SilentlyContinue
+Stop-Process -Name "printfilterpipelinesvc" -Force -ErrorAction SilentlyContinue
+
+# Остановка службы диспетчера печати
+Stop-Service -Name "Spooler" -Force -ErrorAction Stop
+
+# Очистка файлов спулера
+Remove-Item -Path "$env:windir\System32\spool\PRINTERS\*" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:windir\System32\spool\SERVERS\*" -Recurse -Force -ErrorAction SilentlyContinue
+
+# Перезапуск службы диспетчера печати
+Start-Service -Name "Spooler" -ErrorAction Stop
+
+Write-Host "Очередь печати успешно очищена" -ForegroundColor Green
